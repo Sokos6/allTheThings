a85d7720e6a5c78a073f4a73d65a4e7500a7a916
Sokos6 <will.sokolowski@gmail.com>
2017-06-05 15:08:03 -0400

    first angular run

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Model } from "./model";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app works!';
+    @Component({
+        selector: "todo-app",
+        templateUrl: "app.component.html"
+        })
+    export class AppComponent {
+        model = new Model();
+    
+        getName() {
+            return this.model.user;
+    }
+        
+    getTodoItems() {
+        return this.model.items;
+    }
 }
