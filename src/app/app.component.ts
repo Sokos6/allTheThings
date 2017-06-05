5d65fc97f37fd84d95e839ef584d2a7350f38fec
Sokos6 <will.sokolowski@gmail.com>
2017-06-05 15:51:19 -0400

    add items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from "./model";
+import { Model, TodoItem  } from "./model";
 
     @Component({
         selector: "todo-app",
@@ -13,6 +13,12 @@ import { Model } from "./model";
     }
         
     getTodoItems() {
-        return this.model.items;
+            return this.model.items.filter(item => !item.done);
+    }
+        
+    addItem(newItem) {
+        if (newItem != "") {
+        this.model.items.push(new TodoItem(newItem, false));
+        }
     }
 }
